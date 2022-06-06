d1fcded7516fe06ff4f8ebe5a72016a5d57fcdf9
Claudio Cassiano <claudiocassiano@gmail.com>
2022-06-05 21:25:31 -0300

    Criando projeto

---
+++ src/main/java/com/projeto/eteClaudio/ProjetoEteClaudioApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.eteClaudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEteClaudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEteClaudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/eteClaudio/ProjetoEteClaudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.eteClaudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEteClaudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
