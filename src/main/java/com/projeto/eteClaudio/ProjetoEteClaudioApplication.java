801e605d8a3fef39b0c5f8f7f25d9eaf5e84e1a7
Claudio Cassiano <claudiocassiano@gmail.com>
2022-06-07 21:46:48 -0300

    Criando o autoSave no banco

--- src/main/java/com/projeto/eteClaudio/ProjetoEteClaudioApplication.java
+++ src/main/java/com/projeto/eteClaudio/ProjetoEteClaudioApplication.java
@@ -1,13 +1,35 @@
 package com.projeto.eteClaudio;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.projeto.eteClaudio.domain.Usuario;
+import com.projeto.eteClaudio.repositiries.UsuarioRepository;
+
 @SpringBootApplication
-public class ProjetoEteClaudioApplication {
+public class ProjetoEteClaudioApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoEteClaudioApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Usuario usuario4 = new Usuario(null, "Afonso@gmail.com","Afonso", 25894);
+		Usuario usuario5 = new Usuario(null, "Eduardo@gmail.com","Eduardo", 58515);
+		
+		usuarioRepository.saveAll(Arrays.asList(usuario4, usuario5));
+		
+	}
+	
+	
 
 }
